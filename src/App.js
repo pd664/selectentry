4c63d288bb71333fb84cb9c14cc941aefdb42e2d
pd664 <prateek.dixit664@gmail.com>
2021-08-06 00:23:36 +0530

    temp_server_changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
+import Search from './Search'
+import Submit from './Submit'
+import Home from './Home'
+import useToken from './useToken';
+import Logout from './Logout'
+
+
 
 function App() {
+
+  const { token, setToken } = useToken();
+  
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Home />
+      <Logout />
+      <Switch>
+        <Route path ="/home" component={Home} />
+        <Route path ="/login" component={Login} />
+        <Route path ="/search" component={Search} />
+        <Route path ="/submit" component={Submit} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect }  from 'react'
+import Axios from 'axios'
+import './static/Search.css'
+
+const Search = () => {
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState("")
+    const [date, setDate] = useState("")
+    const [copy, setCopy] = useState("")
+    const [data, setData] = useState([])
+
+    const host =  process.env.BACKEND_URL
+
+    const searchRecordByName = () => {
+        Axios.get(`${host}/search/searchRecordByName/${name}`)
+        .then((res) => {
+            setData(res.data)
+        })
+    }
+    useEffect(() => {
+        searchRecordByName()
+    })
+
+    const searchRecordByPhone = () => {
+        Axios.get(`${host}/search/searchRecordByPhone/${phone}`)
+        .then((res) => {
+            setData(res.data)
+        })
+    }
+    useEffect(() => {
+        searchRecordByPhone()
+    })
+
+    const searchRecordByCopy = () => {
+        Axios.get(`${host}/search/searchRecordByCopy/${copy}`)
+        .then((res) => {
+            setData(res.data)
+        })
+    }
+    useEffect(() => {
+        searchRecordByCopy()
+    })
+
+    const searchRecordByDate = () => {
+        Axios.get(`${host}/search/searchRecordByDate/${date}`)
+        .then((res) => {
+            setData(res.data)
+        })
+    }
+    useEffect(() => {
+        searchRecordByDate()
+    })
+
+    
+    return(
+        <>
+        
+        <h1 className="heading">SEARCH A ENTRY</h1>
+        <div className="for">
+        <form className="form">
+            <input type="text" onKeyUp={searchRecordByName} onChange={(e) => {setName(e.target.value)}} placeholder="search by Name"></input> <br/>
+            <input type="number" onKeyUp={searchRecordByPhone} onChange={(e) => {setPhone(e.target.value)}} placeholder="search by Phone no."></input><br/>
+            <input type="number" onKeyUp={searchRecordByCopy} onChange={(e) => {setCopy(e.target.value)}} placeholder="search by copy no."></input><br/>
+            <input type="date" onKeyUp={searchRecordByDate} onChange={(e) => {setDate(e.target.value)}} placeholder="search by date"></input><br/>
+
+            
+        </form>
+        </div>
+        <table className="table table-striped table-dark">
+            <thead>
+                <tr>
+                <th scope="col">NAME</th>
+                <th scope="col">PHONE NO.</th>
+                <th scope="col">COPY</th>
+                <th scope="col">DATE</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(client => (
+                    <tr>
+                        <td>{client.name} </td>
+                        <td>{client.phone} </td>
+                        <td>{client.copy}</td>
+                        <td>{client.date}</td>
+                        
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </>
+    )
+}
+
+export default Search
+
